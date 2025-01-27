0a82fa64335233461b82e690c241fb2092486ef3
larycho <sample@users.noreply.com>
2025-01-27 20:57:40 +0100

    Proste przeglądanie plików

--- main.py
+++ main.py
@@ -5,19 +5,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 
-# Otwieranie obrazu
-path = "Fingerprints/1.bmp"
+import tkinter
+import customtkinter
+from tkinter import filedialog
+from tkinter import Button, Label
 
-file_extension = pathlib.Path(path).suffix
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
-if (file_extension in ['.jpg', '.tif', '.bmp','.png']):
-    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+# Function for opening the file explorer window
+def browse_files():
+    path = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("JPEG files",
+                                                        "*.jpg"),
+                                                        ("BMP files",
+                                                        "*.bmp"),
+                                                        ("TIFF files",
+                                                        "*.tif"),
+                                                        ("PNG files",
+                                                        "*.png"),
+                                                       ("all files",
+                                                        "*.*")))  
+    
+    # Otwieranie obrazu
+    file_extension = pathlib.Path(path).suffix
 
-# Binaryzacja Otsu
-threshold, image_otsu = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)
-# Filtr medianowy
-median = cv2.medianBlur(image_otsu, 5) 
+    if (file_extension in ['.jpg', '.tif', '.bmp','.png']):
+        image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
-# Otwiera okno w przeglądarce z możliwością oglądania, zoomowania zdjęcia, sprawdzania wartości pikseli itp.
-fig = px.imshow(cv2.cvtColor(median, cv2.IMREAD_GRAYSCALE))
-fig.show()
+    image_preprocessed = preprocess(image)
+    browser_display(image_preprocessed)
+
+
+def preprocess(image):
+    threshold, image_otsu = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU)  # Binaryzacja Otsu
+    median = cv2.medianBlur(image_otsu, 5) # Filtr medianowy
+    return median
+
+def browser_display(image):     # Otwiera okno w przeglądarce z możliwością oglądania, zoomowania zdjęcia, sprawdzania wartości pikseli itp.
+    fig = px.imshow(cv2.cvtColor(image, cv2.IMREAD_GRAYSCALE))
+    fig.show()
+
+app = customtkinter.CTk()
+app.geometry("200x200")
+app.title("Odciski palców")
+
+button_explore = Button(app, 
+                        text = "Browse Files",
+                        command = browse_files).place(relx=0.5, rely=0.5, anchor='center')
+
+app.mainloop()
